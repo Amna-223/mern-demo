c6ca1035e66eda8496129d8e59f659c92fe54998
Amna Saeed <aamnahsaeed223@gmail.com>
2026-07-19 15:04:11 +0500

    Connected MongoDB

--- mern-demo/server/index.js
+++ mern-demo/server/index.js
@@ -1,16 +1,35 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String,
+    email: String
+});
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
 });
 
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
     console.log(req.body);
+    await User.create(req.body);
+
     res.json(
         {message: "Data Received successfully!"}
     );
